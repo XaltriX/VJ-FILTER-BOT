bebf88a77707163a421bdad9695a7dc4925ee254
XaltriX <129171425+XaltriX@users.noreply.github.com>
2025-09-02 21:43:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,8 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24955235'))
 API_HASH = environ.get('API_HASH', 'f317b3f7bbe390346d8b46868cff0de8')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
-
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
@@ -150,4 +149,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 # Don't Remove Credit @i_am_yamraj
 # Ask Doubt on telegram @i_am_yamraj
+
 
